6cc65b6fa398a55215307e087fe544fae1f5bad7
CH <ch728@cornell.edu>
2022-03-13 23:07:29 -0400

    update

--- data/03_geno/src/01_filter_raw.sh
+++ data/03_geno/src/01_filter_raw.sh
@@ -2,13 +2,16 @@
 
 bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
  bcftools reheader -h data/maxima_head.txt |
- bcftools annotate --rename-chrs |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" -Ob > data/cmaxima_filt.bcf
+ bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4"  |
+ bcftools annotate --rename-chrs data/cmaxima_chrs.txt -Ob > data/cmaxima_filt.bcf
+
 
 bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
  bcftools reheader -h data/moschata_head.txt |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" -Ob > data/cmoschata_filt.bcf
+ bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" |
+ bcftools annotate --rename-chrs data/cmoschata_chrs.txt -Ob > data/cmoschata_filt.bcf
 
 bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
  bcftools reheader -h data/pepo_head.txt |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" -Ob > data/cpepo_filt.bcf
+ bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" |
+ bcftools annotate --rename-chrs data/cpepo_chrs.txt -Ob > data/cpepo_filt.bcf
