e5cafc646ab35e8528ac80510fa99a84a1535f0e
Chris Hernandez <ch728@cbsurobbins.biohpc.cornell.edu>
2022-03-20 17:11:16 -0400

    update

--- data/03_geno/src/00_prepare.sh
+++ data/03_geno/src/00_prepare.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+# Make base directories
+if [[ ! -d "data" ]]
+then
+  mkdir data
+  mkdir data/raw data/admix data/gwas data/pca data/filtered
+  mkdir data/admix/cpepo data/admix/cmoschata data/admix/cmaxima
+fi
+
+# Download raw genotype data
+wget http://cucurbitgenomics.org/ftp/GBS_SNP/cucurbita/Cmaxima_raw_SNP.vcf.gz -O data/raw/Cmaxima_raw_SNP.vcf.gz
+
+wget http://cucurbitgenomics.org/ftp/GBS_SNP/cucurbita/Cmoschata_raw_SNP.vcf.gz -O data/raw/Cmoschata_raw_SNP.vcf.gz
+
+wget http://cucurbitgenomics.org/ftp/GBS_SNP/cucurbita/Cpepo_raw_SNP.vcf.gz -O data/raw/Cpepo_raw_SNP.vcf.gz
+
+# Clone Genocore repo and clean 
+git clone https://github.com/lovemun/Genocore.git data/genocore
+rm data/genocore/wheat* data/genocore/LICENSE data/genocore/README.md
+rm -rf data/genocore/.git
+
--- data/03_geno/src/01_filter_raw.sh
+++ data/03_geno/src/01_filter_raw.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-# Conservative filter
-bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
- bcftools reheader -h data/maxima_head.txt |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4"  |
- bcftools annotate --rename-chrs data/cmaxima_chrs.txt -Ob > data/cmaxima_filt.bcf
+# Tabix index raw vcfs to prevent annoying eror
 
-
-bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
- bcftools reheader -h data/moschata_head.txt |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" |
- bcftools annotate --rename-chrs data/cmoschata_chrs.txt -Ob > data/cmoschata_filt.bcf
-
-bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
- bcftools reheader -h data/pepo_head.txt |
- bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" |
- bcftools annotate --rename-chrs data/cpepo_chrs.txt -Ob > data/cpepo_filt.bcf
-
-# Strict filter
+# Filtering
 
 bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
- bcftools reheader -h data/maxima_head.txt |
- bcftools view -M2 -m2 -i "MAF >= 0.05 & F_MISSING <= 0.2"  |
- bcftools annotate --rename-chrs data/cmaxima_chrs.txt -Ob > data/cmaxima_strict_filt.bcf
+ bcftools reheader -h ../01_meta/data/maxima_head.txt |
+ bcftools view -M2 -m2 -i "MAF > 0.05  & F_MISSING <= 0.4" |
+ bcftools annotate --rename-chrs ../01_meta/data/cmoschata_chrs.txt -Ob > data/filtered/cmaxima_filt.bcf
 
-bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
- bcftools reheader -h data/moschata_head.txt |
- bcftools view -M2 -m2 -i "MAF >= 0.05  & F_MISSING <= 0.2" |
- bcftools annotate --rename-chrs data/cmoschata_chrs.txt -Ob > data/cmoschata_strict_filt.bcf
-
-bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
- bcftools reheader -h data/pepo_head.txt |
- bcftools view -M2 -m2 -i "MAF >= 0.05 & F_MISSING <= 0.2" |
- bcftools annotate --rename-chrs data/cpepo_chrs.txt -Ob > data/cpepo_strict_filt.bcf
-
+#bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
+# bcftools reheader -h ../01_meta/data/moschata_head.txt |
+# bcftools view -M2 -m2 -i "MAF > 0.05  & F_MISSING <= 0.4" |
+# bcftools annotate --rename-chrs ../01_meta/data/cmoschata_chrs.txt -Ob > data/filtered/cmoschata_filt.bcf
+#
+#bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
+# bcftools reheader -h ../01_meta/data/pepo_head.txt |
+# bcftools view -M2 -m2 -i "MAF > 0.05  & F_MISSING <= 0.4" |
+# bcftools annotate --rename-chrs ../01_meta/data/cpepo_chrs.txt -Ob > data/filtered/cpepo_filt.bcf
+#
+# LD prune to be used for population structure
 
 
