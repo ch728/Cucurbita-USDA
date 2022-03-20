ab02cdbc0701179914db163a85d57141ee64257c
CH <ch728@cornell.edu>
2022-03-20 12:54:09 -0400

    update

--- data/03_geno/src/01_filter_raw.sh
+++ data/03_geno/src/01_filter_raw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Conservative filter
 bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
  bcftools reheader -h data/maxima_head.txt |
  bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4"  |
@@ -15,3 +16,23 @@ bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |
  bcftools reheader -h data/pepo_head.txt |
  bcftools view -M2 -m2 -i "MAF > 0.01  & F_MISSING <= 0.4" |
  bcftools annotate --rename-chrs data/cpepo_chrs.txt -Ob > data/cpepo_filt.bcf
+
+# Strict filter
+
+bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
+ bcftools reheader -h data/maxima_head.txt |
+ bcftools view -M2 -m2 -i "MAF >= 0.05 & F_MISSING <= 0.2"  |
+ bcftools annotate --rename-chrs data/cmaxima_chrs.txt -Ob > data/cmaxima_strict_filt.bcf
+
+bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
+ bcftools reheader -h data/moschata_head.txt |
+ bcftools view -M2 -m2 -i "MAF >= 0.05  & F_MISSING <= 0.2" |
+ bcftools annotate --rename-chrs data/cmoschata_chrs.txt -Ob > data/cmoschata_strict_filt.bcf
+
+bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
+ bcftools reheader -h data/pepo_head.txt |
+ bcftools view -M2 -m2 -i "MAF >= 0.05 & F_MISSING <= 0.2" |
+ bcftools annotate --rename-chrs data/cpepo_chrs.txt -Ob > data/cpepo_strict_filt.bcf
+
+
+
--- data/03_geno/src/07_run_structure_no_cult.sh
+++ data/03_geno/src/07_run_structure_no_cult.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+admix(){
+	admixture -j2 --cv $1 $2 | tee $3/log$2.out
+} 
+
+export -f admix
+
+parallel -j 3  admix data/admix/cpepo/cpepo_nocult.bed {} data/admix/cpepo ::: $(seq 1 15) 
+mv *.Q data/admix/cpepo/
+mv *.P data/admix/cpepo/
+
+parallel -j 3  admix data/admix/cmoschata/cmoschata_nocult.bed {} data/admix/cmoschata ::: $(seq 1 15) 
+mv *.Q data/admix/cmoschata/
+mv *.P data/admix/cmoschata/
+
+parallel -j 3  admix data/admix/cmaxima/cmaxima_nocult.bed {} data/admix/cmaxima ::: $(seq 1 15) 
+mv *.Q data/admix/cmaxima/
+mv *.P data/admix/cmaxima/
+
+# Format output logs
+
+grep -h "CV" data/admix/cpepo/log*.out | cut -d " " -f3,4 | tr "(K=):" ":" | sed s/://g | cut -f1,2 > data/admix/cpepo/cv_error.txt
+
+grep -h "CV" data/admix/cmoschata/log*.out | cut -d " " -f3,4 | tr "(K=):" ":" | sed s/://g | cut -f1,2 > data/admix/cmoschata/cv_error.txt
+
+grep -h "CV" data/admix/cmaxima/log*.out | cut -d " " -f3,4 | tr "(K=):" ":" | sed s/://g | cut -f1,2 > data/admix/cmaxima/cv_error.txt
+
