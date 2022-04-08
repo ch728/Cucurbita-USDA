8745cba36bcbdc2310fdae1c863679b6967d29f9
Chris Hernandez <ch728@cbsurobbins.biohpc.cornell.edu>
2022-04-08 15:55:48 -0400

    update

--- data/03_geno/src/01_filter_raw.sh
+++ data/03_geno/src/01_filter_raw.sh
@@ -3,17 +3,17 @@
 # Filtering
 bcftools view data/raw/Cmaxima_raw_SNP.vcf.gz |  
  bcftools reheader -h ../01_meta/data/maxima_head.txt |
- bcftools view  -M2 -m2 -i "MAF >= 0.01 & F_MISSING <= 0.5" |
+ bcftools view  -M2 -m2 -v snps -i "MAF >= 0.05 & F_MISSING <= 0.4" |
  bcftools annotate --rename-chrs ../01_meta/data/cmaxima_chrs.txt -Ob > data/filtered/cmaxima_filt.bcf
 
 bcftools view data/raw/Cmoschata_raw_SNP.vcf.gz |  
  bcftools reheader -h ../01_meta/data/moschata_head.txt |
- bcftools view -M2 -m2 -i "MAF >= 0.01 & F_MISSING <= 0.5" |
+ bcftools view -M2 -m2 -v snps -i "MAF >= 0.05 & F_MISSING <= 0.4" |
  bcftools annotate --rename-chrs ../01_meta/data/cmoschata_chrs.txt -Ob > data/filtered/cmoschata_filt.bcf
 
 bcftools view data/raw/Cpepo_raw_SNP.vcf.gz |  
  bcftools reheader -h ../01_meta/data/pepo_head.txt |
- bcftools view -s "^HONEYNUT" -M2 -m2 -i "MAF >= 0.01 & F_MISSING <= 0.5" |
+ bcftools view -s "^HONEYNUT" -M2 -m2 -v snps -i "MAF >= 0.05 & F_MISSING <= 0.4" |
  bcftools annotate --rename-chrs ../01_meta/data/cpepo_chrs.txt -Ob > data/filtered/cpepo_filt.bcf
 
 # LD prune to be used for population structure
