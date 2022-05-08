daff7289c9295db8cfcf1cc0533e4201a793aa11
Chris Hernandez <ch728@cbsurobbins.biohpc.cornell.edu>
2022-05-07 22:39:56 -0400

    upate

--- src/06_cult_str_fig.R
+++ src/06_cult_str_fig.R
@@ -1,3 +1,13 @@
 library(tidyverse)
+library(gridExtra)
+
+
+# Read in files
+cpepo <- read.table("data/03_geno/data/admix/cpepo/cpepo_admix_cult.5.Q", sep=" ",
+                    header=F)
+cpepoLab <- read_delim("data/03_geno/data/admix/cpepo/cpepo_admix_cult.fam" , delim=" ",
+                     col_names=F)[,1]
+rownames(cpepo) <- cpepoLab[[1]]
+
 
 
