bdf6081dd40a036bb1091f4e3e03287299ffb395
CH <ch728@cornell.edu>
2022-05-12 20:18:34 -0400

    update

--- data/01_meta/src/01_tidy_meta.R
+++ data/01_meta/src/01_tidy_meta.R
@@ -24,6 +24,12 @@ countryDat$region <- gsub("Mashriq", "Arab States", countryDat$region)
 countryDat$region <- gsub("Arabian Peninsula", "Arab States", countryDat$region)
 countryDat$region <- gsub("Western Africa", "Africa", countryDat$region)
 countryDat$region <- gsub("South East Asia", "Asia", countryDat$region)
+countryDat <- rbind(countryDat, data.frame(country="Afghanistan", region="Asia"))
+countryDat <- rbind(countryDat, data.frame(country="Democratic Republic of the Congo", region="Africa"))
+countryDat <- rbind(countryDat, data.frame(country="Africa", region="Africa"))
+countryDat[countryDat$country=="Australia",2] <- "Australia"
+countryDat <- rbind(countryDat, data.frame(country="Reunion", region="Europe"))
+countryDat <- rbind(countryDat, data.frame(country="Eswatini", region="Africa"))
 
 # Read in files
 pepo <- read_excel("data/raw/C_pepo_GBS_accession.xlsx")
@@ -31,16 +37,12 @@ moschata <- read_excel("data/raw/C_moschata_GBS_accession.xlsx")
 maxima <- read_excel("data/raw/C_maxima_GBS_accession.xlsx")
 
 # Rename columns
-colnames(pepo) <- c("accession_id", "accession_name", "taxonomy", "country", "narrative", "improvement_status", "total_reads")
+colnames(pepo) <- c("accession_id", "accession_name", "taxonomy", "country", "narrative", "cultivar_class", "improvement_status")
 
-colnames(moschata) <- c("accession_id", "accession_name", "taxonomy", "country", "narrative", "improvement_status", "total_reads")
+colnames(moschata) <- c("accession_id", "accession_name", "taxonomy", "country", "narrative", "cultivar_class", "improvement_status")
 
-colnames(maxima) <- c("accession_id", "accession_name", "taxonomy", "country", "narrative", "improvement_status", "total_reads")
-
-# Remove white space from accession Ids
-pepo$accession_id <- gsub(" ", "_", pepo$accession_id)
-moschata$accession_id <- gsub(" ", "_", moschata$accession_id)
-maxima$accession_id <- gsub(" ", "_", maxima$accession_id)
+colnames(maxima) <- c("accession_id", "accession_name", "taxonomy", "country","improvement_status", "cultivar_class",
+"narrative")
 
 # Fix some country names
 pepo$country <- gsub("Russian Federation", "Russia", pepo$country)
@@ -51,6 +53,7 @@ moschata$country <- gsub("Korea", "South Korea", moschata$country)
 
 maxima$country <- gsub("Russian Federation", "Russia", maxima$country)
 maxima$country <- gsub("Korea", "South Korea", maxima$country)
+maxima$country <- gsub("North Macedonia", "Macedonia", maxima$country)
 
 pepo <- merge(pepo, countryDat, by="country", sort=F, all.x=T)
 moschata <- merge(moschata, countryDat, by="country", sort=F, all.x=T)
